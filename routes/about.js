3cec4c58d89b04b5870b9996c753e359bfb5232a
Laura Lachin <lauralachin@10-18-185-25.dynapool.wireless.nyu.edu>
2022-11-01 08:55:14 -0400

    Added router stuff

---
+++ routes/about.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+});
+
+router.get('/', (req, res) => {
+    res.send('<div><h1>About page</h1></div>')
+  });
+
+router.get('/me', (req, res) => {
+    res.send([
+        {
+            "id": "me",
+            "name": "Laura",
+            "message": "Hello"
+        }
+    ])
+});
+
+  module.exports = router;
